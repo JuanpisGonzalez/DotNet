e261aba3475ba80a87fdfa0d353f52caeacbb227
juan pablo gonzalez <jupgonzalezz@gmail.com>
2024-10-03 09:54:11 -0500

    Add users administration

--- DotNetMvcIdentity/Controllers/UserController.cs
+++ DotNetMvcIdentity/Controllers/UserController.cs
@@ -3,9 +3,11 @@ using DotNetMvcIdentity.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DotNetMvcIdentity.Controllers
 {
+    [Authorize(Roles = "Administrator")]
     public class UserController : Controller
     {
         private readonly UserManager<IdentityUser> _userManager;
@@ -16,9 +18,134 @@ namespace DotNetMvcIdentity.Controllers
             _context = context;
         }
 
-        public IActionResult Index()
+        [HttpGet]
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var users = await _context.AppUsers.ToListAsync();
+            var userRoles = await _context.UserRoles.ToListAsync();
+            var roles = await _context.Roles.ToListAsync();
+
+            foreach (var user in users)
+            {
+                var role = userRoles.FirstOrDefault(u => u.UserId == user.Id);
+                if (role == null)
+                {
+                    user.Role = "None";
+                }
+                else
+                {
+                    user.Role = roles.FirstOrDefault(u => u.Id == role.RoleId).Name;
+                }
+            }
+
+            return View(users);
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "Administrator")]
+        public IActionResult UpdateUser(string id)
+        {
+            var userDb = _context.AppUsers.FirstOrDefault(u => u.Id == id);
+            if (userDb == null)
+            {
+                return NotFound();
+            }
+
+            var userRol = _context.UserRoles.ToList();
+            var roles = _context.Roles.ToList();
+            var role = userRol.FirstOrDefault(u => u.UserId == userDb.Id);
+
+            if(role != null)
+            {
+                userDb.IdRol = roles.FirstOrDefault(u => u.Id == role.RoleId).Id;
+            }
+            userDb.Roles = _context.Roles.Select(u => new Microsoft.AspNetCore.Mvc.Rendering.SelectListItem
+            {
+                Text = u.Name,
+                Value = u.Id
+            });
+            return View(userDb);
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Administrator")]
+        public async Task<IActionResult> UpdateUser(AppUser user)
+        {
+            if (ModelState.IsValid)
+            {
+                var userDb = _context.AppUsers.FirstOrDefault(u => u.Id == user.Id);
+                if(userDb == null)
+                {
+                    return NotFound();
+                }
+
+                var userRole = _context.UserRoles.FirstOrDefault(r => r.UserId == user.Id);
+                if (userRole != null)
+                {
+                    var currentRole = _context.Roles.Where(r => r.Id == userRole.RoleId).Select(e => e.Name).FirstOrDefault();
+                    //delete actual role
+                    await _userManager.RemoveFromRoleAsync(userDb, currentRole);
+                }
+
+                //Add user to a new role
+                await _userManager.AddToRoleAsync(userDb, _context.Roles.FirstOrDefault(u => u.Id == user.IdRol).Name);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));    
+            }
+
+            user.Roles = _context.Roles.Select(u => new Microsoft.AspNetCore.Mvc.Rendering.SelectListItem
+            {
+                Text = u.Name,
+                Value = u.Id
+            });
+            return View(user);
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Administrator")]
+        [ValidateAntiForgeryToken]
+        public IActionResult BlockUnblockUser(string id)
+        {
+            var userDb = _context.AppUsers.FirstOrDefault(u => u.Id == id);
+            if(userDb == null)
+            {
+                return NotFound();
+            }
+
+            if(userDb.LockoutEnd != null && userDb.LockoutEnd > DateTime.Now)
+            {
+                TempData["Successful"] = "The user has been unlocked";
+                //is block
+                userDb.LockoutEnd = DateTime.Now;
+
+            }
+            else
+            {
+                TempData["Successful"] = "The user has been locked";
+                //Block user
+                userDb.LockoutEnd = DateTime.Now.AddDays(1);
+            }
+
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Administrator")]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(string id)
+        {
+            var userDb = _context.AppUsers.FirstOrDefault(u => u.Id == id);
+            if (userDb == null)
+            {
+                return NotFound();
+            }
+
+            _userManager.DeleteAsync(userDb);
+
+            TempData["Successful"] = "The user has been deleted";
+            //_context.SaveChanges();
+            return RedirectToAction(nameof(Index));
         }
 
         //Update profile
@@ -36,7 +163,7 @@ namespace DotNetMvcIdentity.Controllers
             {
                 return NotFound();
             }
-
+            TempData["Successful"] = "The user has been updated";
             return View(dbUser);
         }
 
@@ -80,13 +207,15 @@ namespace DotNetMvcIdentity.Controllers
             if (ModelState.IsValid)
             {
                 var user = await _userManager.FindByEmailAsync(email);
-                if (user == null) { 
+                if (user == null)
+                {
                     return View("~/Views/Account/Error.cshtml");
                 }
 
                 var token = await _userManager.GeneratePasswordResetTokenAsync(user);
                 var result = await _userManager.ResetPasswordAsync(user, token, changePasswordViewModel.Password);
-                if (result.Succeeded) {
+                if (result.Succeeded)
+                {
                     return RedirectToAction("ChangePasswordConfirmation");
                 }
                 else
--- DotNetMvcIdentity/Models/AppUser.cs
+++ DotNetMvcIdentity/Models/AppUser.cs
@@ -1,10 +1,15 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Policy;
 
 namespace DotNetMvcIdentity.Models
 {
     public class AppUser: IdentityUser
     {
-        public string? Id {  get; set; }
+       // public string? Id {  get; set; }
         public string Name { get; set; }
         public string Url { get; set; }
         public Int32 CountryCode { get; set; }
@@ -14,5 +19,13 @@ namespace DotNetMvcIdentity.Models
         public string Address { get; set; }
         public DateTime Birthdate { get; set; }
         public bool State {  get; set; }
+        //role assignation
+        [NotMapped]
+        [Display(Name = "user role")]
+        public string IdRol {  get; set; }
+        [NotMapped]
+        public string Role { get; set; }
+        [NotMapped]
+        public IEnumerable<SelectListItem>? Roles { get; set; }
     }
 }
