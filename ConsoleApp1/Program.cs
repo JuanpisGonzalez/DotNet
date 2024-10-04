a7b65aebdbb27b5982916c7148c356bbe7719c8d
juan pablo gonzalez <jupgonzalezz@gmail.com>
2024-10-03 21:23:44 -0500

    Asyncronous c#

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var task1 = new Task(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Asyncronous task 1");
+            });
+
+            task1.Start();
+
+
+            var task2 = new Task(() =>
+            {
+                Thread.Sleep(100);
+                Console.WriteLine("Asyncronous task 2");
+            });
+
+            task2.Start();
+            Console.WriteLine("Doing another things...");
+            await task1;
+            await task2;
+
+            int resultRandom = await RandomAsync(4);
+            Console.WriteLine($"{resultRandom}");
+            Console.WriteLine("Task finished");
+            Console.ReadLine();
+
+        }
+        public static async Task<int> RandomAsync(int num)
+        {
+            var task = new Task<int>(() =>
+
+                new Random().Next() * num
+            );
+
+            task.Start();
+            int result = await task;
+            return result;
+        }
+    }
+}
